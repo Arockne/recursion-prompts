daf5416d1f5e76d857065f249d235887ec16b013
Dan <daniel.joseph.calvin@gmail.com>
2020-09-24 17:27:30 -0600

    Implemented reverseArr function

--- src/recursion.js
+++ src/recursion.js
@@ -200,6 +200,14 @@ var createArray = function(str) {
 
 // 17. Reverse the order of an array
 var reverseArr = function(array) {
+  if (array.length === 0) {
+    return [];
+  }
+
+  var lastElement = array.pop();
+  var reverse = reverseArr(array);
+  reverse.unshift(lastElement);
+  return reverse;
 };
 
 // 18. Create a new array with a given value and length.
